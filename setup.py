f62cd24b31d7fd42e51784039c688d05aa889983
Bruno Mahé <bruno@cloudera.com>
2010-10-04 17:45:30 -0700

    Do not install test module

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url="http://github.com/cloudera/crepo/tree/master",
     maintainer="Chad Metcalf",
     maintainer_email="chad@cloudera.com",
-    py_modules=['crepo', 'git_command', 'git_repo', 'manifest', 'trace', 'test' ],
+    py_modules=['crepo', 'git_command', 'git_repo', 'manifest', 'trace' ],
     entry_points = { 'console_scripts': [ 'crepo = crepo:main', ], },
     install_requires = ['setuptools', 'simplejson'],
 )
